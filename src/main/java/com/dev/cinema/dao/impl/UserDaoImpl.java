3e28f3ff4631e066417e5d686feaf0fe646654ed
ivan <Ivan.krivoruchko.1999@gmail.com>
2020-02-05 22:29:35 +0200

    added user service and authentication service

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,51 @@
+package com.dev.cinema.dao.impl;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.exceptions.DataProcessingExeption;
+import com.dev.cinema.lib.anotations.Dao;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class UserDaoImpl implements UserDao {
+    @Override
+    public User add(User user) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long userId = (Long) session.save(user);
+            transaction.commit();
+            user.setId(userId);
+            return user;
+        } catch(Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingExeption("Can't insert user entity", e);
+        }
+
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<User> criteriaQuery
+                    = criteriaBuilder.createQuery(User.class);
+            Root<User> root = criteriaQuery.from(User.class);
+
+            Predicate equalEmail = criteriaBuilder.equal(root.get("email"), email);
+            criteriaQuery.select(root).where(equalEmail);
+            return session.createQuery(criteriaQuery).uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingExeption("Can't find user by email", e);
+        }
+    }
+}
